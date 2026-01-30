a42b72e48d438ccf7ff4229d80f830ffc07cfb20
SkalskiP <piotr.skalski92@gmail.com>
2026-01-30 15:46:59 +0100

    Fix seqmap_file indexing in MotChallenge2DBox
    Extract first element from seqmap_file when it's returned as a list.

--- trackeval/datasets/mot_challenge_2d_box.py
+++ trackeval/datasets/mot_challenge_2d_box.py
@@ -153,6 +153,7 @@ class MotChallenge2DBox(_BaseDataset):
                     seqmap_file = os.path.join(self.config['GT_FOLDER'], 'seqmaps', self.gt_set + '.txt')
                 else:
                     seqmap_file = os.path.join(self.config["SEQMAP_FOLDER"], self.gt_set + '.txt')
+            seqmap_file = seqmap_file[0]
             if not os.path.isfile(seqmap_file):
                 print('no seqmap found: ' + seqmap_file)
                 raise TrackEvalException('no seqmap found: ' + os.path.basename(seqmap_file))
